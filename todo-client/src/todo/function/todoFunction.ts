beaf43d3b2ea8dc9c9d27a87e2272c87ad1ceae7
RFSub <dhelitus@gmail.com>
2024-02-18 18:38:31 +0900

    refactor httpRequest func

--- todo-client/src/todo/function/todoFunction.ts
+++ todo-client/src/todo/function/todoFunction.ts
@@ -12,7 +12,7 @@ type EditBody = {
 }
 
 // async awaitで処理完了後に次の処理に移れる
-export const sendPostTodo = async (todoContext: string, todoUserName: string): Promise<boolean> => {
+export const sendPostTodo = async (todoContext: string, todoUserName: string) => {
     let isSendResult: boolean = false;
     // 定義から実際に送信するボティを作る
     // 一応勉強のためにType作ったけどこれ必要だったかは怪しい
@@ -21,71 +21,16 @@ export const sendPostTodo = async (todoContext: string, todoUserName: string): P
         post_user_name: todoUserName
     }
 
-    await fetch(APICONF.BASE_ENDPOINT + "create", {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-            // 'Authorization' : TOKEN
-        },
-        body: JSON.stringify(sendBody)
-    }).then((response) => {
-        if (!response.ok) {
-            console.log(response);
-        } else {
-            console.log('クライアントから登録');
-            isSendResult = true;
-        }
-        return response.json();
-    }).then((data) => {
-        console.log(data);
-        // internal serverエラーはこっちでキャッチする
-        if (data.status == 500) {
-            let message = "登録に失敗しました。"
-
-            if (todoContext.length > 500) {
-                message += "\nTODO内容は500文字以下で入力してください。"
-            }
-            if (todoUserName.length > 100) {
-                message += "\nニックネームは100文字以下で入力してください。"
-            }
-            // alertで簡易エラー表示
-            alert(message);
-        }
-    }).catch((error) => {
-        console.log(error);
-    });
+    isSendResult = await sendRequest(APICONF.BASE_ENDPOINT + "create", "POST", sendBody);
 
     return isSendResult;
 }
 
 // 削除はシンプル
-export const sendDeleteTodo = async (id: number): Promise<boolean> => {
+export const sendDeleteTodo = async (id: number) => {
     let isDeleteResult: boolean = false;
 
-    await fetch(APICONF.BASE_ENDPOINT + "delete/" + id.toString(), {
-        method: 'DELETE',
-        headers: {
-            'Content-Type': 'application/json',
-            // 'Authorization' : 'APITOKEN'
-        },
-    }).then((response) => {
-        if (!response.ok) {
-            console.log(response);
-        } else {
-            console.log('クライアントから削除');
-            isDeleteResult = true;
-        }
-        return response.json();
-    }).then((data) => {
-        console.log(data);
-        // internal serverエラーはこっちでキャッチする
-        if (data.status == 500) {
-            alert("登録に失敗しました");
-        }
-    }).catch((error) => {
-        console.log(error);
-    });
-
+    isDeleteResult = await sendRequest(APICONF.BASE_ENDPOINT + "delete/" + id.toString(), "DELETE");
     return isDeleteResult;
 }
 
@@ -98,36 +43,62 @@ export const sendEditTodo = async (id: number, todoContext: string): Promise<boo
         todo_context: todoContext,
     }
 
-    await fetch(APICONF.BASE_ENDPOINT + "update", {
-        method: 'POST',
+    isSendResult = await sendRequest(APICONF.BASE_ENDPOINT + "update", "POST", sendBody);
+
+    return isSendResult;
+}
+
+// ? anyにすることでOption引数になる。型も任意になる。
+const sendRequest = async (url: string, method: string, body?: any) => {
+    let isSendResult: boolean = false;
+
+    await fetch(url, {
+        method: method,
         headers: {
             'Content-Type': 'application/json',
-            // 'Authorization' : TOKEN
         },
-        body: JSON.stringify(sendBody)
+        // bodyがあれば送信に含める。
+        body: body ? JSON.stringify(body) : undefined
     }).then((response) => {
         if (!response.ok) {
             console.log(response);
         } else {
-            console.log('クライアントから編集');
             isSendResult = true;
         }
+        // responseを返すことでdataに値が入っていく
         return response.json();
+
     }).then((data) => {
         console.log(data);
         // internal serverエラーはこっちでキャッチする
         if (data.status == 500) {
-            let message = "登録に失敗しました。"
-
-            if (todoContext.length > 500) {
-                message += "\nTODO内容は500文字以下で入力してください。"
-            }
-            // alertで簡易エラー表示
-            alert(message);
+            handleAlertMessage(method, body);
         }
     }).catch((error) => {
         console.log(error);
     });
 
     return isSendResult;
+}
+
+// 簡易バリデーション的にalert
+// サーバサイドAPIでもリクエストボディにバリデーションはかかる
+const handleAlertMessage = (method: string, body?: any) => {
+    let message = "処理に失敗しました。";
+
+    if (method === "POST") {
+        // POSTならBodyがあるので参照する
+        const todoContext = body.todo_context;
+        // 編集の場合は存在しないので空文字にしておく
+        const todoUserName = body.post_user_name ?? "";
+        if (todoContext.length > 500) {
+            message += "\nTODO内容は500文字以下で入力してください。"
+        }
+        if (todoUserName.length > 100) {
+            message += "\nニックネームは100文字以下で入力してください。"
+        }
+    } else if (method === "DELETE") {
+        message = "削除に失敗しました。";
+    }
+    alert(message);
 }
